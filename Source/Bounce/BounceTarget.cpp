276854d01957e905652a0534e184b849b104dfe0
R0binicus <robinandloki@gmail.com>
2025-05-03 23:47:14 +1000

    🎯Target: Code cleanup
    - Removed of unused and unnecessary code from target scripts

--- Source/Bounce/BounceTarget.cpp
+++ Source/Bounce/BounceTarget.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "BounceTarget.h"
 #include "DrawDebugHelpers.h"
 #include "Components/BoxComponent.h"
@@ -8,52 +7,18 @@
 // Sets default values
 ABounceTarget::ABounceTarget()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
 	MyCollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComp"));
 	MyCollisionBox->SetCollisionProfileName("Target");
 
 	RootComponent = MyCollisionBox;
 
-	//MyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MY MESH"));
-	//MyMesh->SetupAttachment(RootComponent);
-
 	MyCollisionBox->OnComponentBeginOverlap.AddDynamic(this, &ABounceTarget::OnComponentBeginOverlap);
-}
-
-// Called when the game starts or when spawned
-void ABounceTarget::BeginPlay()
-{
-	Super::BeginPlay();
-}
-
-// Called every frame
-void ABounceTarget::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-	//DrawDebugSphere(GetWorld(), GetActorLocaton(), SphereRadius, 20, FColor::Purple, false, -1, 0, 1);
-}
-
-void ABounceTarget::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
-{
-	// Only add impulse and destroy projectile if we hit a physics
-	if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr) && OtherComp->IsSimulatingPhysics())
-	{
-		OtherComp->AddImpulseAtLocation(GetVelocity() * 100.0f, GetActorLocation());
-	}
-
-	UE_LOG(LogClass, Log, TEXT("Target Hit"));
-
-	Destroy();
 }
 
 void ABounceTarget::OnComponentBeginOverlap(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr))
 	{
-		UE_LOG(LogClass, Log, TEXT("Target Overlap"));
 		Destroy();
 	}
 }
--- Source/Bounce/BounceTarget.h
+++ Source/Bounce/BounceTarget.h
@@ -17,26 +17,10 @@ class BOUNCE_API ABounceTarget : public AActor
 	UPROPERTY(VisibleDefaultsOnly, Category = Target)
 	UBoxComponent* MyCollisionBox;
 
-	//UPROPERTY(VisibleAnywhere, Category = Target)
-	//UStaticMeshComponent* MyMesh;
-
 	UFUNCTION()
 	void OnComponentBeginOverlap(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 public:	
 	// Sets default values for this actor's properties
 	ABounceTarget();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	/** called when projectile hits something */
-	UFUNCTION()
-	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
-
 };
