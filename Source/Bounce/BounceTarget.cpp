7b1199b3381b4a7ae7d5f680663a07c4af7edce7
R0binicus <robinandloki@gmail.com>
2025-05-04 16:47:11 +1000

    🎯Target: Changed OnOverlap to OnHit
    - Changed target to use OnHit, rather than overlap
    - Added GetCollisionProfileName check
    - Projectile now considers hitting the target a 'bounce'
    - Updated the collision presets

--- Source/Bounce/BounceTarget.cpp
+++ Source/Bounce/BounceTarget.cpp
@@ -7,20 +7,18 @@
 // Sets default values
 ABounceTarget::ABounceTarget()
 {
-	MyCollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComp"));
-	MyCollisionBox->SetCollisionProfileName("Target");
+	CollisionComp = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComp"));
+	CollisionComp->BodyInstance.SetCollisionProfileName("Target");
+	CollisionComp->OnComponentHit.AddDynamic(this, &ABounceTarget::OnHit);		// set up a notification for when this component hits something blocking
 
-	RootComponent = MyCollisionBox;
-
-	MyCollisionBox->OnComponentBeginOverlap.AddDynamic(this, &ABounceTarget::OnComponentBeginOverlap);
+	RootComponent = CollisionComp;
 }
 
-void ABounceTarget::OnComponentBeginOverlap(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+void ABounceTarget::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
 	if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr))
 	{
+		if (OtherComp->GetCollisionProfileName() != "Projectile") return;
 		Destroy();
 	}
 }
-
-
--- Source/Bounce/BounceTarget.h
+++ Source/Bounce/BounceTarget.h
@@ -13,14 +13,14 @@ class BOUNCE_API ABounceTarget : public AActor
 {
 	GENERATED_BODY()
 
-	/** Box collision component */
-	UPROPERTY(VisibleDefaultsOnly, Category = Target)
-	UBoxComponent* MyCollisionBox;
-
-	UFUNCTION()
-	void OnComponentBeginOverlap(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-
 public:	
 	// Sets default values for this actor's properties
 	ABounceTarget();
+
+	/** Box collision component */
+	UPROPERTY(VisibleDefaultsOnly, Category = Target)
+	UBoxComponent* CollisionComp;
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 };
