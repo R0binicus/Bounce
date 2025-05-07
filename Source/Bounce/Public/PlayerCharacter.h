cb9c0951b2162af3404e0c2b369560fbb9f699e9
Haulidaie <haulidaie@gmail.com>
2025-05-08 03:11:57 +1000

    🏃 Parkour movement basics
    - Complete rewrite of the playable character
    - Added sprinting and sliding actions (no crouching)
    - Customized acceleration, friction, and max speed
    - Increased gravity by 50% and buffed jump velocity
    - Minor changes to map and gamemode for slide testing

--- Source/Bounce/Public/PlayerCharacter.h
+++ Source/Bounce/Public/PlayerCharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "Logging/LogMacros.h"
 #include "PlayerCharacter.generated.h"
 
@@ -12,52 +13,164 @@ class USkeletalMeshComponent;
 class UCameraComponent;
 class UInputAction;
 class UInputMappingContext;
+class UCharacterMovementComponent;
 struct FInputActionValue;
 
-DECLARE_LOG_CATEGORY_EXTERN(LogTemplateCharacter, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogPlayerCharacter, Log, All);
 
 UCLASS(config=Game)
 class BOUNCE_API APlayerCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
-	/** Pawn mesh: 1st person view (arms; seen only by self) */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Mesh, meta = (AllowPrivateAccess = "true"))
+	/** Pawn mesh for first person (arms seen only by self) */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Mesh, meta=(AllowPrivateAccess="true"))
 	USkeletalMeshComponent* Mesh1P;
 
+	/** Character movement component */
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Mesh, meta=(AllowPrivateAccess="true"))
+	UCharacterMovementComponent* CharacterMovement = Cast<UCharacterMovementComponent>(GetCharacterMovement());
+
 	/** First person camera */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera, meta=(AllowPrivateAccess="true"))
 	UCameraComponent* FirstPersonCameraComponent;
 
 	/** MappingContext */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess="true"))
 	UInputMappingContext* DefaultMappingContext;
 
 	/** Jump Input Action */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess="true"))
 	UInputAction* JumpAction;
 
 	/** Move Input Action */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess="true"))
 	UInputAction* MoveAction;
 
 	/** Look Input Action */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess="true"))
 	class UInputAction* LookAction;
 
+	/** Sprint Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess="true"))
+	class UInputAction* SprintAction;
+
+	/** Sprint Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess="true"))
+	class UInputAction* SlideAction;
+
 // Called when the game starts or when spawned
-protected:
-	virtual void BeginPlay() override;
-
-//////////////////////////////////////////////////////////////////////////// Input
-
 public:
-	// Sets default values for this character's properties
 	APlayerCharacter();
 
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	// UFUNCTION()
+	// void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+protected:
+	virtual void BeginPlay() override;
+	/** Called for movement input */
+	void Move(const FInputActionValue& Value);
+
+	/** Called for looking input */
+	void Look(const FInputActionValue& Value);
+
+	/** Called for holding sprint */
+    void Sprint(const FInputActionValue& Value);
+
+	/** Called for releasing sprint */
+    void StopSprinting(const FInputActionValue& Value);
+
+	/** Called for holding slide */
+    void Slide(const FInputActionValue& Value);
+
+	/** Called for releasing slide */
+    void StopSliding(const FInputActionValue& Value);
+
+	/** Maximum speed while walking */
+	UPROPERTY(EditDefaultsOnly, Category="Movement")
+	float MoveSpeedWalk = 600.f;
+
+	/** Maximum speed while sprinting */
+	UPROPERTY(EditDefaultsOnly, Category="Movement")
+	float MoveSpeedSprint = 1200.f;
+
+	/** Maximum speed while sliding */
+	UPROPERTY(EditDefaultsOnly, Category="Movement")
+	float MoveSpeedSlide = 1500.f;
+
+	/** Applied friction while grounded */
+	UPROPERTY(EditDefaultsOnly, Category="Movement")
+	float MoveFrictionGround = 1.5f;
+
+	/** Applied friction while sliding */
+	UPROPERTY(EditDefaultsOnly, Category="Movement")
+	float MoveFrictionSlide = 0.2f;
+
+	/** Applied friction while airborne */
+	UPROPERTY(EditDefaultsOnly, Category="Movement")
+	float MoveFrictionAir = 0.2f;
+
+	/** Applied acceleration while walking */
+	UPROPERTY(EditDefaultsOnly, Category="Movement")
+	float MoveAccelerationWalk = 1024.f;
+
+	/** Applied acceleration while sprinting */
+	UPROPERTY(EditDefaultsOnly, Category="Movement")
+	float MoveAccelerationSprint = 2048.f;
+
+	/** Applied acceleration while sliding */
+	UPROPERTY(EditDefaultsOnly, Category="Movement")
+	float MoveAccelerationSlide = 0.f;
+
+protected:
+	// APawn interface
+	virtual void NotifyControllerChanged() override;
+	virtual void SetupPlayerInputComponent(UInputComponent* InputComponent) override;
+	// End of APawn interface
+
+public:
+	/** Returns Mesh1P subobject **/
+	USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
+	/** Returns FirstPersonCameraComponent subobject **/
+	UCameraComponent* GetFirstPersonCamera() const { return FirstPersonCameraComponent; }
+
+	float CapsuleRadius = 5.f;
+	float CapsuleHeight = 80.f;
+	float CapsuleHalfed = 40.f;
+
+	/** Getter for walking speed */
+	UFUNCTION(BlueprintPure, Category="Movement")
+	FORCEINLINE float GetSpeedWalk() const { return MoveSpeedWalk; }
+
+	/** Getter for sprinting speed */
+	UFUNCTION(BlueprintCallable, Category="Movement")
+	FORCEINLINE float GetSpeedSprint() const { return MoveSpeedSprint; }
+
+	/** Getter for sliding speed */
+	UFUNCTION(BlueprintCallable, Category="Movement")
+	FORCEINLINE float GetSpeedSlide() const { return MoveSpeedSlide; }
+
+	/** Getter for grounded friction */
+	UFUNCTION(BlueprintCallable, Category="Movement")
+	FORCEINLINE float GetFrictionGround() const { return MoveFrictionGround; }
+
+	/** Getter for sliding friction */
+	UFUNCTION(BlueprintCallable, Category="Movement")
+	FORCEINLINE float GetFrictionSlide() const { return MoveFrictionSlide; }
+
+	/** Getter for airborne friction */
+	UFUNCTION(BlueprintCallable, Category="Movement")
+	FORCEINLINE float GetFrictionAir() const { return MoveFrictionAir; }
+
+	/** Getter for walking acceleration */
+	UFUNCTION(BlueprintPure, Category="Movement")
+	FORCEINLINE float GetAccelerationWalk() const { return MoveAccelerationWalk; }
+
+	/** Getter for sprinting acceleration */
+	UFUNCTION(BlueprintCallable, Category="Movement")
+	FORCEINLINE float GetAccelerationSprint() const { return MoveAccelerationSprint; }
+
+	/** Getter for sliding acceleration */
+	UFUNCTION(BlueprintCallable, Category="Movement")
+	FORCEINLINE float GetAccelerationSlide() const { return MoveAccelerationSlide; }
 };
