e3fc75c8f59fd34f5e779b46ea6a3732522138a5
Haulidaie <haulidaie@gmail.com>
2025-05-12 23:25:17 +1000

    🔨 Weapon parts fully integrated
    - Finished adding weapon parts!
    - Won't be able to get any displays working
    - Minor changes to settings
    - Added the weapon part item

--- Source/Bounce/Private/WeaponPart.cpp
+++ Source/Bounce/Private/WeaponPart.cpp
@@ -9,13 +9,17 @@
 // Sets default values for this component's properties
 UWeaponPart::UWeaponPart()
 {
-	
+	PrimaryComponentTick.bCanEverTick = true;
+	//RandomizeValues();
+	SetVisibility(false, false);
 }
 
 bool UWeaponPart::EquipPart(UWeaponComponent* TargetWeapon)
 {
-	if(!Weapon->AttachPart(this)) return false;
-    Weapon = TargetWeapon;
+	if(TargetWeapon == nullptr) return false;
+
+	Weapon = TargetWeapon;
+	if(Weapon->AttachPart(this)) SetVisibility(true, false);
 
 	FDetachmentTransformRules DetachmentRules(EDetachmentRule::KeepRelative, true);
 	DetachFromComponent(DetachmentRules);
@@ -23,6 +27,7 @@ bool UWeaponPart::EquipPart(UWeaponComponent* TargetWeapon)
 	// Attach the weapon to the Player Character
 	FAttachmentTransformRules AttachmentRules(EAttachmentRule::SnapToTarget, true);
 	AttachToComponent(Weapon, AttachmentRules, FName(PartName));
+	Rename(*PartName, Weapon);
 	//SetRelativeLocation(PartOffset);
 
 	return true;
@@ -45,14 +50,21 @@ FRotator UWeaponPart::RandDouble(float max, float min)
 
 void UWeaponPart::RandomizeValues()
 {
-	Scatter = FMath::FRandRange(0.0f, 10.0f);
-	Amount = (int)FMath::RandRange(1, 10);
-	FireRate = FMath::FRandRange(0.0f, 3.0f);
-	RecoilAmount = FMath::FRandRange(0.0f, 7.0f);
-	Damage = FMath::FRandRange(1.0f, 50.0f);
-	Bounces = (int)FMath::RandRange(1, 10);
-	Speed = FMath::FRandRange(100.0f, 10000.0f);
-	Bounciness = FMath::FRandRange(0.1f, 3.0f);
-    GravityAmount = FMath::FRandRange(0.0f, 5.0f);
-	Lifespan = FMath::FRandRange(0.5f, 25.0f);
+	Scatter = FMath::FRandRange(0.0f, 1.0f);
+	Amount = (int)FMath::RandRange(1, 2);
+	FireRate = FMath::FRandRange(-0.01f, -0.05f);
+	RecoilAmount = FMath::FRandRange(0.0f, 1.0f);
+	Damage = FMath::FRandRange(0.1f, 1.0f);
+	Bounces = (int)FMath::RandRange(-1, 5);
+	Speed = FMath::FRandRange(-100.0f, 300.0f);
+	Bounciness = FMath::FRandRange(0.01f, 0.03f);
+    GravityAmount = FMath::FRandRange(-0.3f, 0.03f);
+	Lifespan = FMath::FRandRange(0.1f, 3.0f);
+	float s = FMath::FRandRange(0.01f, 0.03f);
+	Scale = FVector(s);
+}
+
+FString UWeaponPart::DisplayStats() {
+	//FString text = FString::Printf(TEXT("Weapon Part {\n  Scatter += &f\n  Bullets += &d\n  Fire Rate += &f\n  Recoil += &f\n  Damage += &f\n  Bounces += &d\n  Speed += &f\n  Rebound += &f\n  Gravity += &f\n  Lifespan += &f\n"), Scatter, Amount, FireRate, RecoilAmount, Damage, Bounces, Speed, Bounciness, GravityAmount, Lifespan);
+	return(PartName);
 }
